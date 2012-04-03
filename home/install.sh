3fd823010866a6fd72696bcb6437f71a217001fd
Lassi Kortela <lassi@lassi.io>
2012-04-03 19:45:04 +0300

    Fix a bug in home/install.sh. The awk test wouldn't work if lassi appears later than the first line in the file

--- home/install.sh
+++ home/install.sh
@@ -1,10 +1,12 @@
 #! /bin/sh
-cd "$(dirname "$0")"
-cp="cp" # Change this to "echo cp" for testing.
+cp="cp" # Change this to "echo cp" for a dry run.
+cd "$(dirname "$0")" # Make sure the current dir is the script dir.
 for src in dot.* ; do
     dst=~/"$(echo "$src" | sed "s/^dot//")"
-    if ! test -e "$dst".orig && test -s "$dst" && awk "/lassi/ { exit 1 }" < "$dst" ; then
-        # dst.orig doesn't exist; dst exists and doesn't contain my name on the first line
+    if ! test -e "$dst".orig && test -s "$dst" && awk "/lassi/ {exit 1} // {exit}" < "$dst" ; then
+        # dst.orig doesn't exist; dst exists and isn't empty and
+        # doesn't contain my name on the first line
+        # so make a backup of dst into dst.orig
         $cp -pf "$dst" "$dst".orig
     fi
     $cp -pf "$src" "$dst"
