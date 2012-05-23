f33804b5162bf72c6585887dc51af7891acbf2ee
Lassi Kortela <lassi@lassi.io>
2012-05-23 15:49:59 +0300

    Add dashify command to subr-lassi

--- emacs/subr-lassi.el
+++ emacs/subr-lassi.el
@@ -35,3 +35,28 @@
            ((lambda (x) (if (listp x) x (list x)))
             (or (get-char-property (point) 'face) '(default))))
    "*Customize Faces*"))
+
+(defun dashify (dash-char)
+  "Put a line of dashes under the titles on the current line, which should look like a heading or a table header."
+  (interactive (list (if current-prefix-arg ?= ?-)))
+  (goto-char (point-at-eol))
+  (delete-horizontal-space)
+  (goto-char (point-at-bol))
+  (let (titles)
+    (let ((start (point-at-bol)))
+      (while (re-search-forward "[ \t]+" (point-at-eol) t)
+	(unless (equal " " (match-string 0))
+	  (when (< (point-at-bol) (match-beginning 0))
+	    (setq titles (nconc titles (list (cons (- start (point-at-bol)) (- (match-beginning 0) (point-at-bol)))))))
+	  (setq start (match-end 0))))
+      (when (< start (point-at-eol))
+	(setq titles (nconc titles (list (cons (- start (point-at-bol)) (- (point-at-eol) (point-at-bol))))))))
+    (goto-char (point-at-eol))
+    (insert (with-temp-buffer
+	      (insert "\n")
+	      (let ((last 0))
+		(dolist (title titles (buffer-substring (point-min) (point-max)))
+		  (destructuring-bind (start . end) title
+		    (insert (make-string (- start last) ? ))
+		    (insert (make-string (- end start) dash-char))
+		    (setq last end))))))))
