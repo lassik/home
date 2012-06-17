40ecd56dbfa6ae779db1e506ca48f5627258b2e2
Lassi Kortela <lassi@lassi.io>
2012-06-17 23:36:15 +0300

    Improve comments in init.el

--- emacs/init.el
+++ emacs/init.el
@@ -1,17 +1,29 @@
-;;;; Personal Emacs initialization file
-;;;;
-;;;; Put (load "~/path/to/this/init.el") in the file that the variable
-;;;; `user-init-file' on a vanilla emacs points to.
-;;;;
-;;;; Tested on the following platforms.
-;;;;
-;;;; Operating system    Emacs             Emacs build opts
-;;;; ------------------  ------------      ----------------
-;;;; Mac OS X 10.6       Aquamacs 2.4      MULE
-;;;; Ubuntu Linux 11.10  GNU Emacs 22.3.1  MULE, GTK
-;;;; Windows XP          XEmacs            MULE
+;;; Personal Emacs initialization file
 
-;;;; We have landed in an unknown location; let's orient ourselves
+;; Lassi Kortela <lassi@lassikortela.net>
+
+;; To use, append the following line of code to the file that the
+;; variable `user-init-file' on a vanilla emacs points to:
+
+;; (load "~/path/to/this/init.el")
+
+;; The following directory layout is expected:
+
+;; public/code/emacs/  -- non-standard elisp files downloaded from the internet
+;; private/code/emacs/ -- non-standard elisp files I'm editing
+;; private/conf/emacs/ -- personal configuration files for emacs including this file
+
+;; I use this on the following platforms.
+
+;; Emacs             Operating system    GUI
+;; ----------------  ----------------    ---
+;; Aquamacs 2.4      Mac OS X 10.6       Cocoa
+;; GNU Emacs 23.1.1  Ubuntu Linux 10.04  GTK
+;; GNU Emacs 22.3.1  Ubuntu Linux 11.10  GTK
+;; GNU Emacs 23.4.1  Windows XP          Windows
+;; XEmacs 21.5.31    Windows XP          Windows
+
+;;; Find out where our stuff is
 
 (defconst my-emacs-conf-dir
   (expand-file-name (file-name-directory load-file-name)))
@@ -22,14 +34,17 @@
 (defconst my-emacs-private-code-dir
   (expand-file-name (concat my-emacs-conf-dir "../../../private/code/emacs/")))
 
-;;;; Start off by loading some standard stuff
+;;; Load extensions bundled with Emacs
 
-;;; To muffle byte-compiler warnings we need to `require' everything
-;;; that's configured in this file. Another reason to `require' a
-;;; module is to eliminate the load time upon its first use that
-;;; loading it on demand would incur.
+;; We load all extensions immediately for two reasons:
 
-(require 'cl) ; For great justice.
+;; 1) To avoid the byte-compiler warnings about undefined variables
+;; that configuring an unloaded extension would generate.
+
+;; 2) To eliminate the load time that would otherwise be incurred upon
+;; the first use of a load-on-demand extension.
+
+(require 'cl)                           ; For great justice.
 
 (require 'cc-mode)
 (require 'css-mode)
@@ -38,7 +53,7 @@
 (require 'ruby-mode)
 (require 'tex-mode)
 
-;;;; Load extensions that aren't bundled with Emacs
+;;; Load extensions not bundled with Emacs
 
 (dolist (code-dir (list my-emacs-public-code-dir
                         my-emacs-private-code-dir))
@@ -48,10 +63,11 @@
     ;; Kill all byte-compiled .elc files; they are a nuisance. In
     ;; addition to cluttering directory listings they may be out of
     ;; date or made by an incompatible Emacs, and in such cases `load'
-    ;; won't do the right thing, which is to resort to loading the .el
-    ;; file instead.  Note that the `delete-file' call below deletes
-    ;; even such .elc files that have no corresponding .el file. This
-    ;; is not an issue for me since I always keep the .el files.
+    ;; won't do the right thing, which is to resort to loading the
+    ;; corresponding .el file instead.  Note that the `delete-file'
+    ;; call below deletes even such .elc files that have no
+    ;; corresponding .el file. This is not an issue for me since I
+    ;; always keep the .el files.
     (mapc #'delete-file (directory-files code-dir t "\\.elc$"))
 
     ;; Before loading anything, add the extension directory to
@@ -69,12 +85,12 @@
     ;; At last, load the extensions.
     (mapc #'load-file (directory-files code-dir t "\\.el$"))))
 
-;;;; Internationalization and localization
+;;; Internationalization and localization
 
-;;; Default to UTF-8 with Unix newlines everywhere. The present setup
-;;; routine is a mess uncovered through much trial and error, but
-;;; hopefully at some point down the line it will become easier as
-;;; defaults begin to reflect the brave new world of Unicode.
+;; Default to UTF-8 with Unix newlines everywhere. The present setup
+;; routine is a mess uncovered through much trial and error, but
+;; hopefully at some point down the line it will become easier as
+;; defaults begin to reflect the brave new world of Unicode.
 
 (whenhost (unix xemacs)
   (require 'un-define)
@@ -91,7 +107,7 @@
   (set-coding-priority-list '(utf-8))
   (set-language-environment "Latin-1"))
 
-;;;; Unfasten some seatbelts
+;;; Unfasten some seatbelts
 
 (defalias 'yes-or-no-p 'y-or-n-p)
 
@@ -100,7 +116,8 @@
 (put 'narrow-to-region 'disabled nil)
 (set-variable 'initial-scratch-message "")
 
-;; Make the startup screen a blank slate.
+;; Make Emacs startup with a blank scratch buffer instead of a splash
+;; screen.
 (whenhost gnuemacs
   (set-variable 'inhibit-splash-screen t)
   (set-variable 'inhibit-startup-echo-area-message (user-login-name)))
@@ -110,25 +127,32 @@
   (defun my-dummy-ring-bell-function () (interactive))
   (set-variable 'ring-bell-function 'my-dummy-ring-bell-function))
 
-;; A frame title that I can use to distinguish one Emacs from another.
+;; Set a frame title that I can use to distinguish one Emacs from
+;; another. Having the name of the computer in the frame title is
+;; useful because I might operate a remote GUI emacs using SSH X
+;; forwarding.
 (setq frame-title-format
       (concat "%b"
               " " (downcase (substring (system-name) 0 (position ?. (system-name))))
               " " (downcase (replace-regexp-in-string "[^A-Za-z0-9_-].*$" "" invocation-name))))
 
-;;;; Operating system specific fixes
+(whenhost xemacs
+  (set-variable 'menubar-visible-p nil))
 
+;;; Operating system specific fixes
+
+;; This is appended by default to grep command lines and seems plain
+;; and simply set wrong in the default configuration: it points to the
+;; file "/dev/null" that exists only under Unix and Cygwin.
 (whenhost (xemacs windows)
-  ;; This seems plain and simply set wrong (to the value "/dev/null"
-  ;; that exists only under Unix) in the default configuration.
   (set-variable 'grep-null-device ""))
 
-;;; Make Aquamacs have classic Emacs behavior instead of Mac OS X
-;;; style behavior in many respects.  The most important changes are
-;;; that 1) the Mac keyboard's Command key works as the Emacs Meta key
-;;; in commands such as M-x and M-w, which puts much less strain on my
-;;; fingers; and 2) new frames, called windows in standard GUI lingo,
-;;; are never auto-created.
+;; Make Aquamacs have classic Emacs behavior instead of Mac OS X style
+;; behavior in many respects.  The most important changes are that 1)
+;; the Mac keyboard's Command key works as the Emacs Meta key in
+;; commands such as M-x and M-w, which puts much less strain on my
+;; fingers; and 2) new frames, called windows in standard GUI lingo,
+;; are never auto-created.  This code was copied over from Emacswiki.
 (whenhost aquamacs
   (osx-key-mode -1)
   (setq ns-command-modifier 'meta)
@@ -146,13 +170,11 @@
     (normal-erase-is-backspace-mode nil)
     (set-face-inverse-video-p 'mode-line-inactive t)))
 
-;;;; The remainder of this file consists entirely of custamizations
-;;;; that `customize' can't yet handle. `customize' is awesome.
+;; The remainder of this file consists entirely of custamizations that
+;; the Emacs `customize' facility can't yet handle. `customize' is
+;; awesome.
 
-(whenhost xemacs
-  (set-variable 'menubar-visible-p nil))
-
-;;;; Custom hooks
+;;; Custom hooks
 
 (defun my-case-fold-hook ()
   (setq case-fold-search t)
@@ -161,23 +183,23 @@
 (whenhost xemacs
   (add-hook 'dired-mode-hook 'my-case-fold-hook))
 
-;;;; Custom key bindings
+;;; Custom key bindings
 
-;;; In some configurations the minibuffer editing cursor can go into
-;;; the prompt (which is pretty useless and is triggered accidentally
-;;; most of the time), sometimes it's restricted to the user input
-;;; area (which is what I want 100% of the time).  No matter which is
-;;; the case, skip the prompt and find the start of user input.
+;; In some configurations the minibuffer editing cursor can go into
+;; the prompt (which is pretty useless and is triggered accidentally
+;; most of the time), sometimes it's restricted to the user input
+;; area (which is what I want 100% of the time).  No matter which is
+;; the case, skip the prompt and find the start of user input.
 (defun minibuffer-input-start ()
   (or (unless (text-properties-at (point-min)) (point-min))
       (next-property-change (point-min))
       (point-max)))
 
-;;; Binding this extremely useful command to Control-Backspace in the
-;;; minibuffer keymap allows you to backspace text one pathname
-;;; component at a time instead of creeping along one character at a
-;;; time, all the while keeping the kill ring intact unlike
-;;; `backward-kill-word' would.
+;; Binding this extremely useful command to Control-Backspace in the
+;; minibuffer keymap allows you to backspace text one pathname
+;; component at a time instead of creeping along one character at a
+;; time, all the while keeping the kill ring intact unlike
+;; `backward-kill-word' would.
 (defun my-minibuffer-backspace ()
   (interactive)
   (let ((min (minibuffer-input-start)))
@@ -187,8 +209,8 @@
         (while (and (< min (point)) (not delim)) (backward-char))
         (when (< (point) end) (delete-region (point) end))))))
 
-;;; This command replaces the directory listing in the current `dired'
-;;; buffer with a listing of its parent directory.
+;; This command replaces the directory listing in the current `dired'
+;; buffer with a listing of its parent directory.
 (defun my-dired-up ()
   (interactive)
   (find-alternate-file (my-parent-dir default-directory)))
@@ -201,7 +223,7 @@
 
 (defun my-dired-run ()
   (interactive)
-  ;;(dired-get-filename)
+  ;; TODO: (dired-get-filename)
   (call-interactively 'dired-advertised-find-file))
 
 (define-key (current-global-map) [(control h) (return)] 'manual-entry)
@@ -236,9 +258,9 @@
 (whenhost xemacs
   (define-key cssm-mode-map [?{] 'self-insert-command))
 
-;;;; Custom Lisp indentation
+;;; Custom Lisp indentation
 
-;;; I'm not sure this is the right way to do it.
+;; I'm not sure this is the right way to do it, but it seems to work.
 
 (set-variable 'lisp-indent-function 'common-lisp-indent-function)
 
@@ -249,13 +271,13 @@
 (set-lisp-indent 'asm 1)
 (set-lisp-indent 'casequal 1)
 (set-lisp-indent 'dcase 1)
-(set-lisp-indent 'defconst 1) ; Emacs Lisp
-(set-lisp-indent 'defface 1) ; Emacs Lisp
-(set-lisp-indent 'define 1) ; Scheme
+(set-lisp-indent 'defconst 1)           ; Emacs Lisp
+(set-lisp-indent 'defface 1)            ; Emacs Lisp
+(set-lisp-indent 'define 1)             ; Scheme
 (set-lisp-indent 'define-derived-mode 3) ; Emacs Lisp
 (set-lisp-indent 'define-modify-macro 0)
 (set-lisp-indent 'define-symbol-macro 1)
-(set-lisp-indent 'defpackage 1) ; Common Lisp
+(set-lisp-indent 'defpackage 1)         ; Common Lisp
 (set-lisp-indent 'defsubstrate 1)
 (set-lisp-indent 'ecasequal 1)
 (set-lisp-indent 'edcase 1)
@@ -270,12 +292,11 @@
 (set-lisp-indent 'with-output-to-string 0)
 (set-lisp-indent :section 1)
 
-;;;; Pathnames
+;;; Pathnames
 
 (setq ecumenist-db (concat my-emacs-conf-dir "ecumenist"))
-(pushnew (cons "cl-script" 'lisp-mode) interpreter-mode-alist :test #'equal)
 
-;;;; Find the right `customize' data file and load it
+;;; Find the right `customize' data file and load it
 
 (setq custom-file
       (concat my-emacs-conf-dir
@@ -293,13 +314,13 @@
                 (t          "unix"))
               ".el"))
 
-;;; If you try to load your customizations by simply feeding your
-;;; newly-set `custom-file' to `load-file' in this initialization
-;;; file, some of the customizations won't have any effect because
-;;; who-knows-what will override them.  EmacsWiki folk wisdom says
-;;; `window-setup-hook' and `term-setup-hook' are the right places to
-;;; load them.  I don't understand the bizarre complications of the
-;;; Emacs/Aquamacs startup process so I'm going with folk wisdom.  It
-;;; seems to work for me.
+;; If you try to load your customizations by simply feeding your
+;; newly-set `custom-file' to `load-file' in this initialization file,
+;; some of the customizations won't have any effect because
+;; who-knows-what will override them.  EmacsWiki folk wisdom says
+;; `window-setup-hook' and `term-setup-hook' are the right places to
+;; load them.  I don't understand the bizarre complications of the
+;; Emacs/Aquamacs startup process so I'm going with folk wisdom.  It
+;; seems to work for me.
 (when (file-exists-p custom-file)
   (add-hook 'window-setup-hook (lambda () (load-file custom-file))))
