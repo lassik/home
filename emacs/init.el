79e6fe6f08cc4a622b71856afe61ac416e3568ab
Lassi Kortela <lassi@lassi.io>
2014-12-20 17:15:13 +0200

    Add emacs command maximize-emacs, only does anything on x windows right now

--- emacs/init.el
+++ emacs/init.el
@@ -348,3 +348,13 @@
 
 (whenhost unix
   (setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH"))))  ; FFFUUU
+
+(defun maximize-emacs ()
+  (interactive)
+  (whenhost unix
+    (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
+                           '(2 "_NET_WM_STATE_MAXIMIZED_VERT" 0))
+    (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
+                           '(2 "_NET_WM_STATE_MAXIMIZED_HORZ" 0))))
+
+(maximize-emacs)
