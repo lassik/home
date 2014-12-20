1099cbd334944b7a3f2eb448d3c5ba6c45d07a99
Lassi Kortela <lassi@lassi.io>
2014-12-20 17:10:39 +0200

    Apparently some git 1.x versions also issue that lengthy warning about push.default simple. But there's a workaround: use push.default=current until we've fully migrated into the git 2.x world. Sigh.

--- git/configure-git-common.sh
+++ git/configure-git-common.sh
@@ -1,7 +1,8 @@
 # This script is meant to be sourced from the other scripts.
-git config --global --unset push.default || :
-if git --version | grep 'git version 2' >/dev/null ; then
-    git config --global push.default simple
-fi
+
+# change to push.defalut to 'simple' once I no longer use git <1.7.11
+# anywhere. Sigh.
+git config --global push.default current
+
 git config --global user.name "Lassi Kortela"
 git config --global --unset user.email || :
