710dfc39670be5afb5a3bd5c7ac3c50901c00649
Lassi Kortela <lassi@lassi.io>
2014-12-21 23:44:53 +0200

    Emacs changes for RojbOS ubuntu setup

--- emacs/custom-gnuemacs-23-unix.el
+++ emacs/custom-gnuemacs-23-unix.el
@@ -23,14 +23,15 @@
  '(safe-local-variable-values (quote ((Lowercase . Yes) (Base . 10) (Package . XLIB) (Syntax . Common-lisp))))
  '(scroll-bar-mode nil)
  '(scroll-step 1)
- '(tool-bar-mode nil))
+ '(tool-bar-mode nil)
+ '(x-select-enable-clipboard t))
 
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(default ((t (:stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "unknown" :family "Terminus"))))
+ '(default ((t (:stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "unknown"))))
  '(cursor ((t (:background "aquamarine" :foreground "black"))))
  '(font-lock-comment-face ((((class color) (min-colors 88) (background dark)) (:foreground "gray"))))
  '(renamed-face ((((class color)) (:foreground "orange")))))
