4e536a40774ab60a08916b538b98ffb123cd7942
Lassi Kortela <lassi@lassi.io>
2015-03-07 20:13:33 +0200

    Bake git configuration into .myrc system, getting rid of separate conf/git directory

--- git/configure-git-personal.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-set -e
-cd "$(dirname "$0")"
-. ./configure-git-common.sh
-git config --global user.email "lassi@lassikortela.net"
--- home/.myrc/10-git-config.sh
+++ home/.myrc/10-git-config.sh
@@ -1,5 +1,3 @@
-# This script is meant to be sourced from the other scripts.
-
 # Change to push.default to 'simple' once I no longer use git <1.7.11
 # anywhere. Sigh.
 git config --global push.default current
@@ -8,4 +6,4 @@ git config --global push.default current
 git config --global core.pager "less -R"
 
 git config --global user.name "Lassi Kortela"
-git config --global --unset user.email || :
+git config --global user.email "lassi@lassikortela.net"
