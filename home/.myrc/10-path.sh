1d9e9ee16d11fff8922ebb54753f064050fca4aa
Lassi Kortela <lassi@lassi.io>
2015-03-19 11:14:05 +0200

    Add ~/.local/bin/ to PATH (Python's pip install --user puts stuff there)

--- home/.myrc/10-path.sh
+++ home/.myrc/10-path.sh
@@ -1,4 +1,4 @@
-for d in ~/bin ~/oracle/bin /usr/local/sbin /bin /sbin /usr/bin /usr/sbin /usr/X11R6/bin /usr/X11/bin /usr/games ; do
+for d in ~/bin ~/.local/bin/ ~/oracle/bin /usr/local/sbin /bin /sbin /usr/bin /usr/sbin /usr/X11R6/bin /usr/X11/bin /usr/games ; do
     if test -d "$d"; then
         export PATH="$PATH":"$d"
     fi
