5960f238cbe6e439915dd3a4fb07723c1409053e
Lassi Kortela <lassi@lassi.io>
2015-04-03 10:33:27 +0300

    Set BROWSER env var to chromium under X11

---
+++ home/.myrc/10-browser.sh
@@ -0,0 +1,8 @@
+if test "$DISPLAY" != "" ; then
+    for b in chromium-browser ; do
+        if which "$b" >/dev/null 2>&1 ; then
+            export BROWSER="$b"
+            break
+        fi
+    done
+fi
