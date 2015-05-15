b95737e227a362b9de58560c5b3be8588af13a50
Lassi Kortela <lassi@lassi.io>
2015-05-15 22:04:50 +0300

    10-git-config: reindent

--- home/.myrc/10-git-config.sh
+++ home/.myrc/10-git-config.sh
@@ -1,9 +1,9 @@
-# Change to push.default to 'simple' once I no longer use git <1.7.11
-# anywhere. Sigh.
-git config --global push.default current
+    # Change to push.default to 'simple' once I no longer use git <1.7.11
+    # anywhere. Sigh.
+    git config --global push.default current
 
-# Make less display ANSI colors from git output, not "ESC" junk.
-git config --global core.pager "less -R"
+    # Make less display ANSI colors from git output, not "ESC" junk.
+    git config --global core.pager "less -R"
 
-git config --global user.name "Lassi Kortela"
-git config --global user.email "lassi@lassikortela.net"
+    git config --global user.name "Lassi Kortela"
+    git config --global user.email "lassi@lassikortela.net"
