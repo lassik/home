53c91195a197c601eb57adab793cb0fa50e05885
Lassi Kortela <lassi@lassi.io>
2015-06-16 19:57:09 +0300

    Add uname Darwin check to 20-path-osx.sh

--- home-osx/.config/myrc/20-path-osx.sh
+++ home-osx/.config/myrc/20-path-osx.sh
@@ -1,2 +1,9 @@
-export PATH=/usr/local/bin:"$PATH"  # Homebrew shall override OS X native utilities.
-export PATH="$PATH":/usr/local/texlive/2014basic/bin/x86_64-darwin  # mactex BasicTeX distribution on OSX
+if test "$(uname)" = "Darwin"; then
+
+    # Homebrew shall override OS X native utilities
+    export PATH=/usr/local/bin:"$PATH"
+
+    # MacTeX BasicTeX distribution on OSX
+    export PATH="$PATH":/usr/local/texlive/2014basic/bin/x86_64-darwin
+
+fi
