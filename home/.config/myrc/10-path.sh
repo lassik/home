15b4a7403a968371fb63a538e772ceb11d3ee917
Lassi Kortela <lassi@lassi.io>
2016-03-12 01:30:59 +0200

    myrc: put ~/bin-<os>-<arch> directory in PATH if it exists

--- home/.config/myrc/10-path.sh
+++ home/.config/myrc/10-path.sh
@@ -1,5 +1,7 @@
-for d in ~/bin ~/.local/bin/ ~/oracle/bin /usr/local/sbin /bin /sbin /usr/bin /usr/sbin /usr/X11R6/bin /usr/X11/bin /usr/games ; do
-    if test -d "$d"; then
+for d in ~/"bin-$(uname | tr A-Z a-z)-$(uname -m | tr A-Z a-z)" ~/bin \
+           ~/.local/bin/ ~/oracle/bin /usr/local/sbin /bin /sbin \
+           /usr/bin /usr/sbin /usr/X11R6/bin /usr/X11/bin /usr/games ; do
+    if [ -d "$d" ]; then
         export PATH="$PATH":"$d"
     fi
 done
