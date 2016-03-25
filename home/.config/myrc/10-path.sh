40f7e6406ae27f9c4e980d46bddc6ec0c22ad249
Lassi Kortela <lassi@lassi.io>
2016-03-25 17:09:49 +0200

    myrc: personal bin directories now under ~/.local

--- home/.config/myrc/10-path.sh
+++ home/.config/myrc/10-path.sh
@@ -1,4 +1,4 @@
-for d in ~/"bin-$(uname | tr A-Z a-z)-$(uname -m | tr A-Z a-z)" ~/bin \
+for d in ~/".local/$(uname | tr A-Z a-z)-$(uname -m | tr A-Z a-z)/bin" \
            ~/.local/bin/ ~/oracle/bin /usr/local/sbin /bin /sbin \
            /usr/bin /usr/sbin /usr/X11R6/bin /usr/X11/bin /usr/games ; do
     if [ -d "$d" ]; then
