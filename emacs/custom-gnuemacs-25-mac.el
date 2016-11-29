870e79fec4cedbbe0f437dd4a01e0bbe25bf4caf
Lassi Kortela <lassi@lassi.io>
2016-11-29 19:52:40 +0200

    Make emacs/custom-gnuemacs-25-mac.el
    Copy of emacs/custom-gnuemacs-24-mac.el

---
+++ emacs/custom-gnuemacs-25-mac.el
@@ -0,0 +1,38 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ack-and-a-half-executable "ack")
+ '(blink-cursor-mode nil)
+ '(c-basic-offset 4)
+ '(c-default-style "bsd")
+ '(clojure-defun-style-default-indent t)
+ '(clojure-indent-style t)
+ '(coffee-tab-width 2)
+ '(column-number-mode t)
+ '(cua-mode nil nil (cua-base))
+ '(cursor-type (quote box))
+ '(custom-enabled-themes (quote (sanityinc-tomorrow-night)))
+ '(custom-safe-themes
+   (quote
+    ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "28ec8ccf6190f6a73812df9bc91df54ce1d6132f18b4c8fcc85d45298569eb53" default)))
+ '(dired-listing-switches "-alh")
+ '(editorconfig-custom-hooks (quote (editorconfig-domain-specific)))
+ '(editorconfig-mode t)
+ '(iswitchb-mode t)
+ '(make-backup-files nil)
+ '(ns-alternate-modifier (quote none))
+ '(ns-command-modifier (quote meta))
+ '(package-archives
+   (quote
+    (("gnu" . "http://elpa.gnu.org/packages/")
+     ("melpa" . "https://melpa.org/packages/"))))
+ '(sort-fold-case t t)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
