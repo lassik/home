d5b06d020d02bde31353b92296151d4b82b78f50
Lassi Kortela <lassi@lassi.io>
2016-12-05 12:25:10 +0200

    Don't use emacsclient

--- home/.config/myrc/10-editor.sh
+++ home/.config/myrc/10-editor.sh
@@ -1,8 +1,7 @@
 for e in jmacs mg nano vi ; do
     if which "$e" >/dev/null 2>&1 ; then
         export EDITOR="$e"
+        export ALTERNATE_EDITOR="$EDITOR"
         break
     fi
 done
-export ALTERNATE_EDITOR="$EDITOR"
-which emacsclient >/dev/null 2>&1 && export EDITOR=emacsclient
