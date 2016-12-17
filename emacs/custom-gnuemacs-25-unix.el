096537867ede68d55d435489abfd2042e6eb9438
Lassi Kortela <lassi@lassi.io>
2016-12-17 16:51:19 +0000

    Update Emacs custom file

--- emacs/custom-gnuemacs-25-unix.el
+++ emacs/custom-gnuemacs-25-unix.el
@@ -4,6 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
+ '(ido-mode (quote both) nil (ido))
  '(make-backup-files nil)
  '(package-archives
    (quote
