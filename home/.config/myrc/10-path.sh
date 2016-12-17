af0657e8c3afe21c014c8eb94e71e84432d3f812
Lassi Kortela <lassi@lassi.io>
2016-12-17 18:28:23 +0200

    Remove ~/oracle/bin

--- home/.config/myrc/10-path.sh
+++ home/.config/myrc/10-path.sh
@@ -1,5 +1,5 @@
 for d in ~/".local/$(uname | tr A-Z a-z)-$(uname -m | tr A-Z a-z)/bin" \
-           ~/.local/bin/ ~/oracle/bin /usr/local/sbin /bin /sbin \
+           ~/.local/bin/ /usr/local/sbin /bin /sbin \
            /usr/bin /usr/sbin /usr/X11R6/bin /usr/X11/bin /usr/games ; do
     if [ -d "$d" ]; then
         export PATH="$PATH":"$d"
