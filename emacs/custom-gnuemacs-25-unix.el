94073fd920cd4ef0e30028526b402c89d6f469ea
Lassi Kortela <lassi@lassi.io>
2017-04-29 19:40:13 +0300

    Update Emacs custom file

--- emacs/custom-gnuemacs-25-unix.el
+++ emacs/custom-gnuemacs-25-unix.el
@@ -5,17 +5,20 @@
  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
  '(ido-mode (quote both) nil (ido))
+ '(indent-tabs-mode nil)
  '(make-backup-files nil)
+ '(menu-bar-mode nil)
  '(package-archives
    (quote
     (("gnu" . "http://elpa.gnu.org/packages/")
      ("melpa" . "https://melpa.org/packages/"))))
  '(package-selected-packages
    (quote
-    (yaml-mode ag editorconfig editorconfig-custom-majormode editorconfig-domain-specific ess cider))))
+    (clojure-mode yaml-mode ag editorconfig editorconfig-custom-majormode editorconfig-domain-specific ess cider)))
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 112 :width normal :foundry "PfEd" :family "DejaVu Sans Mono")))))
