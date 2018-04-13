c6c413770ddc27c5ab8de87f981ad794604e2c94
Lassi Kortela <lassi@lassi.io>
2018-04-13 10:01:12 +0300

    Remove MacTeX PATH

--- home/.config/myrc/20-path-osx.sh
+++ home/.config/myrc/20-path-osx.sh
@@ -3,7 +3,4 @@ if [ "$(uname)" = Darwin ]; then
     # Homebrew shall override OS X native utilities
     export PATH=/usr/local/bin:"$PATH"
 
-    # MacTeX BasicTeX distribution on OSX
-    export PATH="$PATH":/usr/local/texlive/2014basic/bin/x86_64-darwin
-
 fi
