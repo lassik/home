a8352f0212cc45ffb683680f891a32bf7231ff26
Lassi Kortela <lassi@lassi.io>
2018-04-14 11:05:01 +0300

    Reformat

--- home/.config/myrc/01-path.sh
+++ home/.config/myrc/01-path.sh
@@ -1,6 +1,13 @@
 for d in ~/".local/$(uname | tr A-Z a-z)-$(uname -m | tr A-Z a-z)/bin" \
-           ~/.local/bin/ /usr/local/sbin /bin /sbin \
-           /usr/bin /usr/sbin /usr/X11R6/bin /usr/X11/bin /usr/games ; do
+           ~/.local/bin/ \
+           /usr/local/sbin \
+           /bin \
+           /sbin \
+           /usr/bin \
+           /usr/sbin \
+           /usr/X11R6/bin \
+           /usr/X11/bin \
+           /usr/games ; do
     if [ -d "$d" ]; then
         export PATH="$PATH":"$d"
     fi
