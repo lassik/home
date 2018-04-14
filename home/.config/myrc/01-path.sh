625504476a894d188b35a3d4c3cd94044b42ca64
Lassi Kortela <lassi@lassi.io>
2018-04-14 11:07:30 +0300

    Put /bin and /sbin before local stuff in PATH
    But keep /usr/local before /usr. So we can override some old system
    software (just not the very core ones in /bin and /sbin).

--- home/.config/myrc/01-path.sh
+++ home/.config/myrc/01-path.sh
@@ -1,11 +1,11 @@
 for d in ~/".local/$(uname | tr A-Z a-z)-$(uname -m | tr A-Z a-z)/bin" \
            ~/.local/bin/ \
+           /bin \
+           /sbin \
            /usr/local/bin \
            /usr/local/sbin \
            /usr/pkg/bin \
            /usr/pkg/sbin \
-           /bin \
-           /sbin \
            /usr/bin \
            /usr/sbin \
            /usr/X11R6/bin \
