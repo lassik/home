ed12a990369d5fed10057f73ac67024b93d4d47d
Lassi Kortela <lassi@lassi.io>
2018-04-14 07:54:43 +0300

    Make sure 'l' and 'c' are not shell aliases
    If they are defined as shell aliases in system-wide shell
    initialization file, then I get a syntax error when trying to redefine
    them as functions.

--- home/.config/myrc/10-aliases.sh
+++ home/.config/myrc/10-aliases.sh
@@ -1,6 +1,8 @@
 # I don't like having my own shell aliases and functions but these are
 # almost mandatory.
 
+unalias l c >/dev/null 2>&1
+
 if which dircolors >/dev/null 2>&1 ; then
     l() {
         ls --color -halF "$@"
