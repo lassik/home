8e48d92d7b5d899580fbae7fe13e4110c3b0a714
Lassi Kortela <lassi@lassi.io>
2018-04-19 13:03:27 +0300

    Simplify prompt for BSDs shipping with ancient shells

--- .shrc.d/95-shellprompt.sh
+++ .shrc.d/95-shellprompt.sh
@@ -2,6 +2,9 @@ case "$-" in
 *i*)
 	# This is an interactive shell.
 	export PS1='\n\w\n\u@\h\$ '
+	case "$(uname)" in
+	DragonFly | FreeBSD | NetBSD) export PS1="${PWD}\$ " ;;
+	esac
 	if which shellprompt >/dev/null 2>&1; then
 		if [ -n "$ZSH_VERSION" ]; then
 			eval "$(shellprompt activate zsh)"
