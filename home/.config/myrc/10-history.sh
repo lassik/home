6cde8cc7a275418088af4c72a6905354ec9528fb
Lassi Kortela <lassi@lassi.io>
2018-04-19 10:19:08 +0300

    Use rm instead of find to delete nohup.out

--- home/.config/myrc/10-history.sh
+++ home/.config/myrc/10-history.sh
@@ -5,4 +5,4 @@ unset HISTFILE
 
 # Clear all readline command history files for shells, interpreters, etc.
 find ~ -maxdepth 1 -type f -name '.*_history' -delete
-find ~ -maxdepth 1 -type f -name 'nohup.out' -delete
+rm -f ~/nohup.out
