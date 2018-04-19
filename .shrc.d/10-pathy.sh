0c1339d4ae3e4ac76cfc5129e18836a3332149c0
Lassi Kortela <lassi@lassi.io>
2018-04-19 15:12:02 +0300

    Remove now-redundant checks for interactive shell

--- .shrc.d/10-pathy.sh
+++ .shrc.d/10-pathy.sh
@@ -1,8 +1,3 @@
-case "$-" in
-*i*)
-	# This is an interactive shell.
-	if which pathy >/dev/null 2>&1 && [ -n "$BASH_VERSION" ]; then
-		eval "$(pathy activate)"
-	fi
-	;;
-esac
+if which pathy >/dev/null 2>&1 && [ -n "$BASH_VERSION" ]; then
+	eval "$(pathy activate)"
+fi
--- .shrc.d/95-shellprompt.sh
+++ .shrc.d/95-shellprompt.sh
@@ -1,16 +1,11 @@
-case "$-" in
-*i*)
-	# This is an interactive shell.
-	PS1='\n\w\n\u@\h\$ '
-	case "$(uname)" in
-	DragonFly | FreeBSD | NetBSD) PS1="${PWD}\$ " ;;
-	esac
-	if which shellprompt >/dev/null 2>&1; then
-		if [ -n "$ZSH_VERSION" ]; then
-			eval "$(shellprompt activate zsh)"
-		elif [ -n "$BASH_VERSION" ]; then
-			eval "$(shellprompt activate bash)"
-		fi
-	fi
-	;;
+PS1='\n\w\n\u@\h\$ '
+case "$(uname)" in
+DragonFly | FreeBSD | NetBSD) PS1="${PWD}\$ " ;;
 esac
+if which shellprompt >/dev/null 2>&1; then
+	if [ -n "$ZSH_VERSION" ]; then
+		eval "$(shellprompt activate zsh)"
+	elif [ -n "$BASH_VERSION" ]; then
+		eval "$(shellprompt activate bash)"
+	fi
+fi
