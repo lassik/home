615655e241bb12af71606d771c5573c84a0be436
Lassi Kortela <lassi@lassi.io>
2018-04-19 12:06:40 +0300

    Delete per-user pkgsrc configuration for NetBSD
    Package management should be configured globally, not per-user.

--- .shrc.d/10-pkgsrc.sh
+++
@@ -1,4 +0,0 @@
-# pkgsrc needs to be told where to download packages on operating systems that use it.
-if [ "$(uname)" = NetBSD ]; then
-    export PKG_PATH="ftp://ftp.netbsd.org/pub/pkgsrc/packages/$(uname)/$(uname -m)/$(uname -r)/All/"
-fi
