768c8f29e4437b510931fccc792d84d5934ee8e8
Lassi Kortela <lassi@lassi.io>
2018-04-19 13:07:07 +0300

    Don't needlessly export PS1 prompt variable

--- .shrc.d/95-shellprompt.sh
+++ .shrc.d/95-shellprompt.sh
@@ -1,9 +1,9 @@
 case "$-" in
 *i*)
 	# This is an interactive shell.
-	export PS1='\n\w\n\u@\h\$ '
+	PS1='\n\w\n\u@\h\$ '
 	case "$(uname)" in
-	DragonFly | FreeBSD | NetBSD) export PS1="${PWD}\$ " ;;
+	DragonFly | FreeBSD | NetBSD) PS1="${PWD}\$ " ;;
 	esac
 	if which shellprompt >/dev/null 2>&1; then
 		if [ -n "$ZSH_VERSION" ]; then
