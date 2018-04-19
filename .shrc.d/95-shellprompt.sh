f35f6b5231236d7a2d5fc8a61c4e53a9a30f4158
Lassi Kortela <lassi@lassi.io>
2018-04-19 12:55:02 +0300

    Add quotes

--- .shrc.d/95-shellprompt.sh
+++ .shrc.d/95-shellprompt.sh
@@ -3,9 +3,9 @@ case "$-" in
 	# This is an interactive shell.
 	if which shellprompt >/dev/null 2>&1; then
 		if [ -n "$ZSH_VERSION" ]; then
-			eval $(shellprompt activate zsh)
+			eval "$(shellprompt activate zsh)"
 		elif [ -n "$BASH_VERSION" ]; then
-			eval $(shellprompt activate bash)
+			eval "$(shellprompt activate bash)"
 		fi
 	else
 		export PS1='\n\w\n\u@\h\$ '
