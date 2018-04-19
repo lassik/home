a8c721910f48a444c9a9f69b22d61cfd286ae737
Lassi Kortela <lassi@lassi.io>
2018-04-19 14:59:20 +0300

    Only load pathy on bash

--- .shrc.d/10-pathy.sh
+++ .shrc.d/10-pathy.sh
@@ -1,7 +1,7 @@
 case "$-" in
 *i*)
 	# This is an interactive shell.
-	if which pathy >/dev/null 2>&1; then
+	if which pathy >/dev/null 2>&1 && [ -n "$BASH_VERSION" ]; then
 		eval "$(pathy activate)"
 	fi
 	;;
