d31e5d0a6efc946ad6098d967a57b040b67bfc0b
Lassi Kortela <lassi@lassi.io>
2018-04-19 12:14:14 +0300

    Combine Homebrew init into one shell script

--- .shrc.d/10-homebrew.sh
+++ .shrc.d/10-homebrew.sh
@@ -1,3 +1,5 @@
 if [ "$(uname)" = Darwin ]; then
 	export HOMEBREW_CASK_OPTS="--appdir=/Applications"
+	# Homebrew shall override OS X native utilities
+	export PATH=/usr/local/bin:"$PATH"
 fi
--- .shrc.d/20-path-osx.sh
+++
@@ -1,6 +0,0 @@
-if [ "$(uname)" = Darwin ]; then
-
-	# Homebrew shall override OS X native utilities
-	export PATH=/usr/local/bin:"$PATH"
-
-fi
