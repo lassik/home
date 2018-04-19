dcdeda13f673cce1598108ceecc9bf4ecfd9c651
Lassi Kortela <lassi@lassi.io>
2018-04-19 12:58:30 +0300

    Set fallback prompt before fancy one

--- .shrc.d/95-shellprompt.sh
+++ .shrc.d/95-shellprompt.sh
@@ -1,14 +1,13 @@
 case "$-" in
 *i*)
 	# This is an interactive shell.
+	export PS1='\n\w\n\u@\h\$ '
 	if which shellprompt >/dev/null 2>&1; then
 		if [ -n "$ZSH_VERSION" ]; then
 			eval "$(shellprompt activate zsh)"
 		elif [ -n "$BASH_VERSION" ]; then
 			eval "$(shellprompt activate bash)"
 		fi
-	else
-		export PS1='\n\w\n\u@\h\$ '
 	fi
 	;;
 esac
