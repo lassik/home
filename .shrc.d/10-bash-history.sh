f741ffa95383b208a2ae0470eaf3a356c33dcd5e
Lassi Kortela <lassi@lassi.io>
2018-04-20 14:09:18 +0300

    Don't conditionalize bash history variables
    They do no harm on non-bash shells, and may even be supported by them.

--- .shrc.d/10-bash-history.sh
+++ .shrc.d/10-bash-history.sh
@@ -1,5 +1,2 @@
-if [ -n "$BASH_VERSION" ]; then
-	export HISTCONTROL=ignoredups
-	# Don't preserve shell command history across sessions.
-	unset HISTFILE
-fi
+HISTCONTROL=ignoredups
+unset HISTFILE
