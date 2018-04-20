75f0a02eba2de1d702dd2f739431ed49a47b3b05
Lassi Kortela <lassi@lassi.io>
2018-04-20 13:42:43 +0300

    Reorganize homebrew scripts

--- .shrc.d/10-bash-completion.sh
+++ .shrc.d/10-bash-completion.sh
@@ -3,8 +3,4 @@ if [ -n "$BASH_VERSION" ]; then
 		. ~/etc/profile.d/bash_completion.sh
 		complete -F _cd -o nospace c
 	fi
-
-	if which brew >/dev/null 2>&1 && [ -f "$(brew --prefix)/etc/bash_completion" ]; then
-		. "$(brew --prefix)/etc/bash_completion"
-	fi
 fi
--- .shrc.d/10-homebrew.sh
+++ .shrc.d/10-homebrew.sh
@@ -1,4 +1,8 @@
-if [ "$(uname)" = Darwin ]; then
+if [ "$(uname)" = Darwin ] && which brew >/dev/null 2>&1; then
+	x="$(brew --prefix)"
 	# Homebrew shall override OS X native utilities
-	export PATH=/usr/local/bin:"$PATH"
+	export PATH="$x/bin:$PATH"
+	if [ -n "$BASH_VERSION" ] && [ -f "$x/etc/bash_completion" ]; then
+		. "$x/etc/bash_completion"
+	fi
 fi
