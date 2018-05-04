9a9cddfaa0a5428129131e5f22d40ce38891ffa5
Lassi Kortela <lassi@lassi.io>
2018-05-05 01:59:38 +0300

    Don't run 'brew --prefix'
    It's too slow. Assume the prefix is always /usr/local.

--- .shrc.d/10-homebrew.sh
+++ .shrc.d/10-homebrew.sh
@@ -1,8 +1,7 @@
 if [ "$(uname)" = Darwin ] && which brew >/dev/null 2>&1; then
-	x="$(brew --prefix)"
-	# Homebrew shall override OS X native utilities
-	export PATH="$x/bin:$PATH"
-	if [ -n "$BASH_VERSION" ] && [ -f "$x/etc/bash_completion" ]; then
-		. "$x/etc/bash_completion"
+	if [ -n "$BASH_VERSION" ] && [ -f /usr/local/etc/bash_completion ]; then
+		. /usr/local/etc/bash_completion
 	fi
+	# Homebrew shall override OS X native utilities
+	export PATH="/usr/local/bin:$PATH"
 fi
