5a3b58d49a825f96d0b7e620a1877139b0e90a34
Lassi Kortela <lassi@lassi.io>
2018-05-16 23:46:19 +0300

    Move homebrew bash completion to 10-bash-completion.sh

--- .shrc.d/10-bash-completion.sh
+++ .shrc.d/10-bash-completion.sh
@@ -1,4 +1,9 @@
-if [ -n "$BASH_VERSION" ] && [ -e ~/etc/profile.d/bash_completion.sh ]; then
-	. ~/etc/profile.d/bash_completion.sh
+if [ -n "$BASH_VERSION" ]; then
+	if [[ -f ~/etc/profile.d/bash_completion.sh ]]; then
+		. ~/etc/profile.d/bash_completion.sh
+	fi
+	if [[ -f /usr/local/etc/bash_completion ]]; then
+		. /usr/local/etc/bash_completion
+	fi
 	complete -F _cd -o nospace c
 fi
--- .shrc.d/10-homebrew.sh
+++ .shrc.d/10-homebrew.sh
@@ -1,7 +1,4 @@
 if [ "$(uname)" = Darwin ] && which brew >/dev/null 2>&1; then
-	if [ -n "$BASH_VERSION" ] && [ -f /usr/local/etc/bash_completion ]; then
-		. /usr/local/etc/bash_completion
-	fi
 	# Homebrew shall override OS X native utilities
 	export PATH="/usr/local/bin:$PATH"
 fi
