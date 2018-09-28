f1c3355ac9ec1c8bef76dd8d5607458a08db0738
Lassi Kortela <lassi@lassi.io>
2018-09-28 17:25:07 +0300

    Update Emacs custom file

--- .emacs.d/custom-gnuemacs-26-mac.el
+++ .emacs.d/custom-gnuemacs-26-mac.el
@@ -13,16 +13,18 @@
  '(column-number-mode t)
  '(cua-mode nil nil (cua-base))
  '(cursor-type (quote box))
- '(custom-enabled-themes (quote (sanityinc-tomorrow-night)))
+ '(custom-enabled-themes (quote (exotica)))
  '(custom-safe-themes
    (quote
-    ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "28ec8ccf6190f6a73812df9bc91df54ce1d6132f18b4c8fcc85d45298569eb53" default)))
+    ("0bff60fb779498e69ea705825a2ca1a5497a4fccef93bf3275705c2d27528f2f" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "28ec8ccf6190f6a73812df9bc91df54ce1d6132f18b4c8fcc85d45298569eb53" default)))
  '(dired-listing-switches "-alh")
  '(editorconfig-custom-hooks (quote (editorconfig-domain-specific)))
  '(editorconfig-mode t)
  '(ido-mode (quote both) nil (ido))
  '(indent-tabs-mode nil)
  '(make-backup-files nil)
+ '(mouse-wheel-progressive-speed nil)
+ '(mouse-wheel-scroll-amount (quote (1 ((shift) . 5) ((control)))))
  '(ns-alternate-modifier (quote none))
  '(ns-command-modifier (quote meta))
  '(package-archives
@@ -32,7 +34,7 @@
  '(package-selected-packages
    (quote
     (0xc cycle-quotes vue-mode elixir-mode shampoo nov swiper ivy crystal-mode kotlin-mode protobuf-mode markdown-mode string-inflection company-go fsharp-mode flow-minor-mode graphql-mode jsx-mode less-css-mode rjsx-mode scss-mode typescript-mode typing typing-game vue-html-mode json-mode json-reformat json-snatcher dtrt-indent github-stars neotree nasm-mode neato-graph-bar maxframe powerline format-all academic-phrases itail rust-mode exotica-theme exec-path-from-shell pyimport slime prettier-js dad-joke d-mode xah-reformat-code web-beautify suomalainen-kalenteri sicp rufo elisp-format rubocop bbcode-mode package-lint lsp-mode lsp-ocaml lsp-python lush-theme lusty-explorer caml flycheck-ocaml merlin tuareg utop js-format web-mode-edit-element web-narrow-mode clang-format py-autopep8 midi-kbd browse-kill-ring csv csv-mode sokoban flycheck flycheck-elm flycheck-gometalinter coffee-mode lua-mode pcre2el php-mode undo-tree docker dockerfile-mode haskell-mode editorconfig cider ess-R-data-view ess-R-object-popup yaml-mode vkill shm rainbow-delimiters purescript-mode nrepl-sync nrepl-eval-sexp-fu moonscript matlab-mode markdown-toc markdown-mode+ magit-rockstar magit-push-remote magit-gitflow magit-gh-pulls magit-find-file lice htmlize hindent haml-mode go-mode flycheck-package flycheck-hdevtools flycheck-haskell flycheck-ghcmod flycheck-clojure fill-column-indicator ess elm-yasnippets elm-mode editorconfig-domain-specific editorconfig-custom-majormode color-theme-solarized color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized clj-refactor cider-tracing cider-spy cider-profile cider-eval-sexp-fu cider-decompile cider-browse-ns autotetris-mode ascii anything ansible-doc ansible aggressive-indent aggressive-fill-paragraph ag afternoon-theme adoc-mode achievements ac-octave)))
- '(sort-fold-case t)
+ '(sort-fold-case t t)
  '(tool-bar-mode nil)
  '(yahoo-weather-location "Helsinki")
  '(yahoo-weather-mode t)
@@ -42,4 +44,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "Black" :foreground "White" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "nil" :family "Menlo")))))
+ )
