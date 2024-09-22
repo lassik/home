fee6a7855cd7d62a6c663cb77a77062f38457d17
Lassi Kortela <lassi@lassi.io>
2024-09-22 15:12:41 +0300

    Disable homebrew PATH

--- .shrc.d/10-homebrew.sh
+++ .shrc.d/10-homebrew.sh
@@ -1,4 +1,5 @@
 if [ "$(uname)" = Darwin ] && which brew >/dev/null 2>&1; then
+    :
 	# Homebrew shall override OS X native utilities
-	export PATH="/usr/local/bin:$PATH"
+	#export PATH="/usr/local/bin:$PATH"
 fi
