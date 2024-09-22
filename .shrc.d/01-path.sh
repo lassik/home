2e1a80d0b98ebf6805c63bafdf2fcb6b0e1a92e9
Lassi Kortela <lassi@lassi.io>
2024-09-22 15:10:23 +0300

    Fortify 01-path.sh

--- .shrc.d/01-path.sh
+++ .shrc.d/01-path.sh
@@ -1,9 +1,5 @@
-for x in ~/".local/$(uname | tr A-Z a-z)-$(uname -m | tr A-Z a-z)/bin" \
-	~/.local/bin \
-	/bin \
+for x in /bin \
 	/sbin \
-	/usr/local/bin \
-	/usr/local/sbin \
 	/usr/pkg/bin \
 	/usr/pkg/sbin \
 	/usr/bin \
@@ -13,5 +9,19 @@ for x in ~/".local/$(uname | tr A-Z a-z)-$(uname -m | tr A-Z a-z)/bin" \
 	/usr/games; do
 	if [ -d "$x" ]; then
 		export PATH="$PATH":"$x"
+	fi
+done
+if [ "$(uname)" = Darwin ] && which brew >/dev/null 2>&1; then
+	# Homebrew shall override OS X native utilities
+	for x in /usr/local/bin /usr/local/sbin; do
+		if [ -d "$x" ]; then
+			export PATH="$x":"$PATH"
+		fi
+	done
+fi
+for x in ~/".local/$(uname | tr A-Z a-z)-$(uname -m | tr A-Z a-z)/bin" \
+	~/.local/bin; do
+	if [ -d "$x" ]; then
+		export PATH="$x":"$PATH"
 	fi
 done
